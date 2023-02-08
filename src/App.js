2a68c9fb5d0190a9de6183f27f0c9074338a06e0
sahillather002 <sahillather002@gmail.com>
2023-02-08 21:28:42 +0530

    skills added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import AboutMe from './components/AboutMe';
 import Hero from './components/Hero';
+import MySkills from './components/MySkills';
 
 function App() {
   return (
     <>
     <Hero />
     <AboutMe />
+    <MySkills />
     </>
   );
 }
