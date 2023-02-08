432e2e88b07fc88d842a2b3eb7bad2843297e7b4
sahillather002 <sahillather002@gmail.com>
2023-02-08 20:43:55 +0530

    about me section is added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
+import AboutMe from './components/AboutMe';
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <>
+    <AboutMe />
+    </>
   );
 }
 
